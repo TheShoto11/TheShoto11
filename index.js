f1b18c2b2c6db3b73f7cc0bd008e259ad3c5a14c
TheShoto11 <121756669+TheShoto11@users.noreply.github.com>
2024-06-22 16:33:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,43 @@ function makeSound(key) {
       kick.play();
       break;
 
+       switch (key) {
+    case "W":
+      var tom1 = new Audio("sound/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "A":
+      var tom2 = new Audio("sound/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "S":
+      var tom3 = new Audio('sound/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "D":
+      var tom4 = new Audio('sound/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "J":
+      var snare = new Audio('sound/snare.mp3');
+      snare.play();
+      break;
+
+    case "K":
+      var crash = new Audio('sound/crash.mp3');
+      crash.play();
+      break;
+
+    case "L":
+      var kick = new Audio('sound/kick-bass.mp3');
+      kick.play();
+      break;
+
+
 
     default: console.log(key);
 
